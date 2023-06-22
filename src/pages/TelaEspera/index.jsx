32380ef0a77e648e5d5da70f00fb748c2b1644fc
marcelooliveiramendes <marcelooliveiraah09@gmail.com>
2023-06-22 10:29:14 -0300

    adicionado a imagem de festa

--- src/pages/TelaEspera/index.jsx
+++ src/pages/TelaEspera/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useState } from 'react';
-import bgCayo from '../../img/cayo.png'
-import bgTaps from '../../img/taps.png'
-import bgErik from '../../img/erik.png'
+// import bgCayo from '../../img/cayo.png'
+// import bgTaps from '../../img/taps.png'
+// import bgErik from '../../img/erik.png'
 import festa from '../../img/festa.png'
-import logo from '../../img/logo.png'
+// import logo from '../../img/logo.png'
 import {motion} from 'framer-motion'
 
 export const TelaEspera = () => {
@@ -29,10 +29,9 @@ export const TelaEspera = () => {
             {/* {count === 1 && (<img src={bgCayo} alt='' className='imageBackgrond'  cache-control="max-age=2592000" />)}
             {count === 2 && (<img src={bgTaps} alt='' className='imageBackgrond'  cache-control="max-age=2592000" />)}
             {count === 3 && (<img src={bgErik} alt='' className='imageBackgrond'  cache-control="max-age=2592000" />)} */}
-            <img src={bgErik} alt='' className='imageBackgrond'  cache-control="max-age=2592000" />
             <div className='containerText'>
                 {/* <h2>Toque na tela para iniciar</h2> */}
-                <img src={festa} alt="" srcset="" />
+              <img src={festa} alt='' className='imageBackgrond'  cache-control="max-age=2592000" />
             </div>
         </motion.div>
 
